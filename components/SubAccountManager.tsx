3cd57e9a05661ff028a98c0991aa87070896048d
SalvadorM <josesalvador0102@gmail.com>
2025-03-03 16:26:41 +0000

    asd

--- components/SubAccountManager.tsx
+++ components/SubAccountManager.tsx
@@ -325,9 +325,6 @@ export default function SubAccountManager({ mode, onSuccess, onCancel }: SubAcco
         <Dialog.Overlay className="fixed inset-0 bg-black/50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
         <Dialog.Content 
           className="fixed left-[50%] top-[50%] z-50 grid w-full max-w-4xl translate-x-[-50%] translate-y-[-50%] gap-4 border bg-white p-0 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] dark:bg-slate-900 sm:rounded-lg"
-          onPointerDownOutside={(e) => {
-            e.preventDefault();
-          }}
         >
           <div className="flex flex-col space-y-1.5 p-6">
             <div className="flex items-center justify-between">
